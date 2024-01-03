6deac3888dc781cfea4d733a9df57ac7281eaae2
gamer00801 <91865766+gamer00801@users.noreply.github.com>
2024-01-03 18:48:49 +0800

    Create LAB07_make.py

---
+++ FM631A_ESP32-AIoT/範例程式/LAB07_make.py
@@ -0,0 +1,35 @@
+import esp32
+import time
+import network     # 匯入network模組
+import urequests   # 匯入urequests模組
+
+# MAKE網址
+url = 'MAKE請求網址'
+
+# 連線至無線網路
+sta=network.WLAN(network.STA_IF)
+sta.active(True)
+# 更換無線網路名稱、密碼 
+sta.connect('無線網路名稱','無線網路密碼')  
+
+while not sta.isconnected():
+    pass
+
+print('Wi-Fi連線成功')
+
+while True:
+    # 霍爾感測值
+    hall = esp32.hall_sensor()
+    print(hall)
+    # 如果磁鐵距離太遠
+    if(hall<100 and hall>0):
+        print("發送警訊!!!!")
+        res = urequests.get(url)
+        if(res.status_code == 200):
+            print("傳送成功")
+        else:
+            print("傳送失敗")
+            print("錯誤碼：",res.status_code)
+        res.close()
+        time.sleep(10)
+    time.sleep(0.1)
